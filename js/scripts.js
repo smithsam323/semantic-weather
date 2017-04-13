482df5b5e90877f545d15e2f6acba35a2ef37f88
smithsam323 <ssmith110@vcd-ceb131-10.eastern.ewu.edu>
2017-04-13 16:41:03 -0700

    Version 1
    * Added JQuery
    * Added Simple Weather

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,24 @@
-// Scripts
-console.log('Hello World');
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+
+$.simpleWeather({
+    location: 99004,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);    
+      $('.image img').attr( 'src', weather.image );   $('.description p').text(weather.text);     
+        
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
+//Accordion Go
+$('.ui.accordion')
+.accordion();
